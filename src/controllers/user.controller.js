2c679e6fbeabda062f175898805c9037e81090ac
Juan Camilo <jcjdlt@hotmail.com>
2023-04-14 21:20:02 -0500

    Se realiza la consulta para mostrar las citas pendientes en cada perfil

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,8 @@
+import { pool } from "../database.js";
+
 export const renderUserProfile = async (req, res, next) => {
-  res.render("profile");
+  const [rows] = await pool.query("SELECT a.id,u.fullname,a.date,a.start_time,n.name,n.price,ap.state FROM appointment a JOIN users u ON a.id_user = u.id JOIN nails n ON a.id_nails = n.id JOIN appointment_state ap ON a.id_state = ap.id WHERE ap.state = 'pendiente' AND a.id_user = ?", [req.user.id]);
+  res.render("profile", {
+    appointment: rows,
+  });
 };
