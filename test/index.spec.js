6cbee607edaa565f34caf72e4dbe4f91839529c0
Juan Camilo <jcjdlt@hotmail.com>
2023-04-27 17:05:16 -0500

    Se modifica el index

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,6 +4,6 @@ import request from "supertest";
 describe("GET /", () => {
     test("deberia responder con un estado 200",async () =>{
         const response = await request(app).get("/").send();
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(201);
     })
 });
