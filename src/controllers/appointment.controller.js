9cd051cc307438f97d23995093d7fdda5f14551e
Juan Camilo <jcjdlt@hotmail.com>
2023-05-01 21:45:23 -0500

    Se modifica la lista de todas las citas

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -2,7 +2,6 @@ import { pool } from "../database.js";
 import { sumTime, getFechaActual, getHoraActual } from "../lib/helpers.js";
 
 export const renderAppointments = async (req, res, next) => {
-    await pool.query("UPDATE appointment set id_state = 3 WHERE date < ? AND id_state = 1", [getFechaActual()]);
     const [rows] = await pool.query("SELECT a.id,u.fullname,a.date,a.start_time,n.name,n.price,ap.state FROM appointment a JOIN users u ON a.id_user = u.id JOIN nails n ON a.id_nails = n.id JOIN appointment_state ap ON a.id_state = ap.id WHERE ap.state = 'pendiente'");
     res.render("appointment/list", {
         appointment: rows
