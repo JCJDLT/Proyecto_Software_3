c53a38b238f420a6b8f5787c934f407e46802e59
Juan Camilo <jcjdlt@hotmail.com>
2023-05-10 11:29:47 -0500

    Se modifica codifica la query de la url

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,14 +1,15 @@
 import { validationResult } from 'express-validator';
 
 export const validator = (req, res, next) => {
-    const { fullname, email, phone} = req.body;
+    const { fullname, email, phone } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        req.flash('errors', errors.array().map(error => error.msg));
-        return res.status(400).redirect(req.originalUrl+"?fullname=" + fullname +"&email="+ email + "&phone="+ phone);
+      req.flash('errors', errors.array().map(error => error.msg));
+      const queryString = `?fullname=${encodeURIComponent(fullname)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
+      return res.status(400).redirect(req.originalUrl + queryString);
     }
     next();
-}
+  };  
 
 export const validatorApointment = (req, res, next) => {
     const errors = validationResult(req);
