abb008b0dde6748f54f52444c96bac44f538b60a
Juan Camilo <jcjdlt@hotmail.com>
2023-05-10 15:53:31 -0500

    Se corrigen error de seguridad

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -24,10 +24,10 @@ export const validatorApointment = (req, res, next) => {
 export const validatorEditApointment = (req, res, next) => {
     const errors = validationResult(req);
     const id = req.params.id;
-    console.log(id)
     if (!errors.isEmpty()) {
         req.flash('errors', errors.array().map(error => error.msg));
-        return res.status(400).redirect(id);
+        const queryString = `${encodeURIComponent(id)}`;
+        return res.status(400).redirect("/appointment/edit/" + queryString);
     }
     next();
 }
