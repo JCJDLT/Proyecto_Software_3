cb1a231d25864d65cc49fffc5716972f7ad94db4
Juan Camilo <jcjdlt@hotmail.com>
2023-05-10 11:52:18 -0500

    Se valida la url antes de redireccionar al usuario

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -4,18 +4,23 @@ export const validator = (req, res, next) => {
     const { fullname, email, phone } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      req.flash('errors', errors.array().map(error => error.msg));
-      const queryString = `?fullname=${encodeURIComponent(fullname)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
-      return res.status(400).redirect(req.originalUrl + queryString);
+        req.flash('errors', errors.array().map(error => error.msg));
+        console.log(req.originalUrl)
+        const queryString = `?fullname=${encodeURIComponent(fullname)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
+        if (req.originalUrl.startsWith("http://localhost:4100/")) {
+            return res.status(400).redirect(req.originalUrl + queryString);
+        }
     }
     next();
-  };  
+};
 
 export const validatorApointment = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         req.flash('errors', errors.array().map(error => error.msg));
-        return res.status(400).redirect(req.originalUrl);
+        if (req.originalUrl.startsWith("http://localhost:4100/")) {
+            return res.status(400).redirect(req.originalUrl);
+        }
     }
     next();
 }
