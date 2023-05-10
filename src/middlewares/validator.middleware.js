ebff21878ae191f50b3b452ad1595d66d3837ee9
Juan Camilo <jcjdlt@hotmail.com>
2023-05-10 12:50:18 -0500

    Se solucionan problemas de Revision de seguridad

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -3,11 +3,16 @@ import { validationResult } from 'express-validator';
 export const validator = (req, res, next) => {
     const { fullname, email, phone } = req.body;
     const errors = validationResult(req);
+
+    const protocol = req.protocol; // Protocolo utilizado: http o https
+    const host = req.get('host'); // Nombre de dominio y puerto
+    const url = `${protocol}://${host}${req.originalUrl}`; // URL completa
+
     if (!errors.isEmpty()) {
         req.flash('errors', errors.array().map(error => error.msg));
-        console.log(req.originalUrl)
+        console.log(url)
         const queryString = `?fullname=${encodeURIComponent(fullname)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
-        if (req.originalUrl.startsWith("http://localhost:4100/")) {
+        if (url.startsWith("https://ec2-100-27-12-164.compute-1.amazonaws.com/")) {
             return res.status(400).redirect(req.originalUrl + queryString);
         }
     }
@@ -16,9 +21,15 @@ export const validator = (req, res, next) => {
 
 export const validatorApointment = (req, res, next) => {
     const errors = validationResult(req);
+
+    const protocol = req.protocol; // Protocolo utilizado: http o https
+    const host = req.get('host'); // Nombre de dominio y puerto
+    const url = `${protocol}://${host}${req.originalUrl}`; // URL completa
+
     if (!errors.isEmpty()) {
         req.flash('errors', errors.array().map(error => error.msg));
-        if (req.originalUrl.startsWith("http://localhost:4100/")) {
+        console.log(url)
+        if (url.startsWith("https://ec2-100-27-12-164.compute-1.amazonaws.com/")) {
             return res.status(400).redirect(req.originalUrl);
         }
     }
